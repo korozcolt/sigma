a151a703b72d548b1a1d60ae991189de19a25fa8
Kronnos <ing.korozco@gmail.com>
2023-02-11 00:25:17 -0500

    First Step

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,61 @@
+<!-- Generator: Adobe Illustrator 25.2.1, SVG Export Plug-In  -->
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" {{ $attributes }}
+    viewBox="0 0 101 169.5" style="overflow:visible;enable-background:new 0 0 101 169.5;" xml:space="preserve">
+    <style type="text/css">
+        .st0 {
+            fill: url(#SVGID_1_);
+        }
+
+        .st1 {
+            fill: url(#SVGID_2_);
+        }
+    </style>
+    <defs>
+    </defs>
+    <linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="0" y1="84.775" x2="101"
+        y2="84.775">
+        <stop offset="0" style="stop-color:#CCE0F4" />
+        <stop offset="6.405089e-02" style="stop-color:#C3DCF2" />
+        <stop offset="0.1706" style="stop-color:#A9D2EE" />
+        <stop offset="0.3066" style="stop-color:#80C2E7" />
+        <stop offset="0.4649" style="stop-color:#47ACDE" />
+        <stop offset="0.5674" style="stop-color:#1E9CD7" />
+        <stop offset="0.8652" style="stop-color:#0075BE" />
+        <stop offset="0.9944" style="stop-color:#005B97" />
+    </linearGradient>
+    <path class="st0"
+        d="M0,75.6L101,0v32.2c0,2.4-0.4,4.6-1.2,6.6c-0.8,2-2.2,3.8-4.2,5.2L49.2,78.1c-1.9,1.4-4,2.7-6.2,3.8
+	c-2.2,1.1-4.5,2-6.9,2.9c2.4,0.8,4.7,1.8,6.9,2.8c2.2,1,4.2,2.3,6.2,3.8l46.3,34.1c2,1.4,3.4,3.1,4.2,5.2c0.8,2,1.2,4.3,1.2,6.8
+	v32.2L0,93.8V75.6z" />
+</svg>
+<!-- Generator: Adobe Illustrator 25.2.1, SVG Export Plug-In  -->
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+    y="0px" width="101px" height="169.5px" viewBox="0 0 101 169.5"
+    style="overflow:visible;enable-background:new 0 0 101 169.5;" xml:space="preserve">
+    <style type="text/css">
+        .st0 {
+            fill: url(#SVGID_1_);
+        }
+
+        .st1 {
+            fill: url(#SVGID_2_);
+        }
+    </style>
+    <defs>
+    </defs>
+    <linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="0" y1="84.775" x2="101"
+        y2="84.775">
+        <stop offset="0" style="stop-color:#CCE0F4" />
+        <stop offset="6.405089e-02" style="stop-color:#C3DCF2" />
+        <stop offset="0.1706" style="stop-color:#A9D2EE" />
+        <stop offset="0.3066" style="stop-color:#80C2E7" />
+        <stop offset="0.4649" style="stop-color:#47ACDE" />
+        <stop offset="0.5674" style="stop-color:#1E9CD7" />
+        <stop offset="0.8652" style="stop-color:#0075BE" />
+        <stop offset="0.9944" style="stop-color:#005B97" />
+    </linearGradient>
+    <path class="st0"
+        d="M0,75.6L101,0v32.2c0,2.4-0.4,4.6-1.2,6.6c-0.8,2-2.2,3.8-4.2,5.2L49.2,78.1c-1.9,1.4-4,2.7-6.2,3.8
+	c-2.2,1.1-4.5,2-6.9,2.9c2.4,0.8,4.7,1.8,6.9,2.8c2.2,1,4.2,2.3,6.2,3.8l46.3,34.1c2,1.4,3.4,3.1,4.2,5.2c0.8,2,1.2,4.3,1.2,6.8
+	v32.2L0,93.8V75.6z" />
+</svg>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html x-data="data" lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'SIGMA') }}</title>
+
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('/apple-icon-57x57.png') }}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('/apple-icon-60x60.png') }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('/apple-icon-72x72.png') }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('/apple-icon-76x76.png') }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('/apple-icon-114x114.png') }}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('/apple-icon-120x120.png') }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('/apple-icon-144x144.png') }}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('/apple-icon-152x152.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-icon-180x180.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('/android-icon-192x192.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('/favicon-96x96.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('/manifest.json') }}">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="{{ asset('/ms-icon-144x144.png') }}">
+    <meta name="theme-color" content="#ffffff">
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Scripts -->
+    <script src="{{ asset('js/init-alpine.js') }}"></script>
+    @include('sweetalert::alert')
+</head>
+
+<body>
+    <div class="flex h-screen bg-gray-50" :class="{ 'overflow-hidden': isSideMenuOpen }">
+        <!-- Desktop sidebar -->
+        @include('layouts.navigation')
+        <!-- Mobile sidebar -->
+        <!-- Backdrop -->
+        @include('layouts.navigation-mobile')
+        <div class="flex flex-col flex-1 w-full">
+            @include('layouts.top-menu')
+            <main class="h-full overflow-y-auto">
+                <div class="container px-6 mx-auto grid">
+                    @if (isset($header))
+                        <h2 class="my-6 text-2xl font-semibold text-gray-700">
+                            {{ $header }}
+                        </h2>
+                    @endif
+
+                    {{ $slot }}
+                </div>
+            </main>
+        </div>
+    </div>
+</body>
+
+</html>
