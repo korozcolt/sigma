2ca842a0c7759b85efa6d3c338f4c1314527b0d1
KristianOrozco <kristian.orozco@infracommerce.lat>
2023-03-19 14:35:28 -0500

    status change class

--- resources/views/components/modal-reviewer-confirmation.blade.php
+++ resources/views/components/modal-reviewer-confirmation.blade.php
@@ -18,9 +18,10 @@
                     <div class="sm:flex sm:items-start">
                         <div
                             class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                            <svg class="h-6 w-6 text-red-600" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                            <svg class="h-6 w-6 text-green-600" stroke="currentColor" fill="none"
+                                viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M6 18L18 6M6 6l12 12"></path>
+                                    d="M5 13l4 4L19 7"></path>
                             </svg>
                         </div>
 
@@ -32,26 +33,31 @@
                     </div>
                 </div>
 
-                <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse w-full">
                     <form action="{{ $route }}" method="post">
                         @csrf
                         @method('put')
-                        <input type="hidden" name="id" value="{{ $id }}">
-                        <select id="status" name="status">
+                        <input type="hidden" name="id" value="{{ $id }}"
+                            class="py-2 px-4 rounded-l-lg border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white w-full">
+                        <select id="status" name="status"
+                            class="form-select select2 mt-1 border-gray-300 rounded-md shadow-sm focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 focus-within:text-primary-600 w-full">
                             <option value="pendiente">Pendiente</option>
                             <option value="revisado">Revisado</option>
                         </select>
-                        <button type="submit"
-                            class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm">
-                            Actualizar
-                        </button>
+                        <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                            <button type="submit"
+                                class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm">
+                                Actualizar
+                            </button>
+                            <button type="button"
+                                class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                                @click="showModal = false">
+                                Cancelar
+                            </button>
+                        </div>
                     </form>
 
-                    <button type="button"
-                        class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                        @click="showModal = false">
-                        Cancelar
-                    </button>
+
                 </div>
             </div>
         </div>
