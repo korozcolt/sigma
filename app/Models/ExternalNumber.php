c35ef163f8e6213ce6d248bd5f8b5fcdeb021da2
Kristian Orozco <81984702+korozcolt@users.noreply.github.com>
2023-09-26 09:52:47 -0500

    [KO]/New data from censo colombia

--- app/Models/ExternalNumber.php
+++ app/Models/ExternalNumber.php
@@ -4,8 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ExternalNumber extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "cedula",
+        "departamento",
+        "municipio",
+        "puesto",
+        "mesa",
+        "direccion",
+        "nombre_completo"
+    ];
+
+    public function setNombreCompletoAttribute($value): void
+    {
+        $this->attributes['nombre_completo'] = strtoupper($value);
+    }
+
+    public function getNombreCompletoAttribute($value): string
+    {
+        return strtoupper($value);
+    }
+
+    //cedula is related to the voter table on dni column
+    public function voter(): BelongsTo
+    {
+        return $this->belongsTo(Voter::class, 'cedula', 'dni');
+    }
+
+
 }
---
+++ resources/views/voters/public/show.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Scripts -->
+    <script src="{{ asset('js/init-alpine.js') }}"></script>
+    <title>Listado</title>
+</head>
+<body>
+<div class="container mx-auto">
+    <div class="m-4"><h1>Listado de Votantes</h1></div>
+
+    <table class="w-full table-auto">
+        <thead>
+        <tr class="bg-gray-800 text-white">
+            <th class="py-2 px-4">Nombre Completo</th>
+            <th class="py-2 px-4">DNI</th>
+            <th class="py-2 px-4">Teléfono</th>
+            <th class="py-2 px-4">Lider</th>
+            <th class="py-2 px-4">Lugar de votación</th>
+            <th class="py-2 px-4">Estado</th>
+        </tr>
+        </thead>
+        <tbody>
+        @forelse ($voters as $voter)
+            <tr class="border-b">
+                <td class="py-2 px-4 text-center">{{ $voter->full_name }}</td>
+                <td class="py-2 px-4 text-center">{{ $voter->dni }}</td>
+                <td class="py-2 px-4 text-center">{{ $voter->phone }}</td>
+                <td class="py-2 px-4 text-center">{{ $voter->leader->full_name }}</td>
+                <td class="py-2 px-4 text-center">{{ $voter->puesto }} - {{ $voter->mesa }}
+                </td>
+
+                <td class="py-2 px-4 text-center">
+                                <span @class([
+                                    'rounded-md px-2 py-1 text-white',
+                                    'bg-orange-600' => $voter->status->pendiente(),
+                                    'bg-green-600' => $voter->status->revisado(),
+                                ])>{{ $voter->status->getLabelText() }}</span>
+                </td>
+            </tr>
+        @empty
+            <tr>
+                <td class="py-2 px-4" colspan="6">No hay Votantes registrados</td>
+            </tr>
+        @endforelse
+        </tbody>
+    </table>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+@yield('scripts')
+</body>
+</html>
